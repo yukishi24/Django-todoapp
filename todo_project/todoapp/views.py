57e392aecbc4b5ddf52c4901284b51720a17ff56
yukishi24 <yukishi24github@gmail.com>
2025-02-20 23:46:28 +0900

    create ListView

--- todo_project/todoapp/views.py
+++ todo_project/todoapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse("<h1>Hello Django!!!</h1>")
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
