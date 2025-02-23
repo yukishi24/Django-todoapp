03dd20000d4c12f0b3e257dea56ee513f7294cc3
yukishi24 <yukishi24github@gmail.com>
2025-02-23 17:47:47 +0900

    Revert "create login"
    This reverts commit a786d686fdeba232ea4ea10b26a770bc335e1cb2.

--- todo_project/todoapp/urls.py
+++ todo_project/todoapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskListLoginView, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name="tasks"),
     path('taskDetail/<int:pk>/', TaskDetail.as_view(), name="task"),
     path('create-task/', TaskCreate.as_view(), name="create-task"),
     path('edit-task/<int:pk>/', TaskUpdate.as_view(), name="edit-task"),
-    path('delete-task/<int:pk>/', TaskDelete.as_view(), name="delete-task"),
-    path('login/', TaskListLoginView.as_view(), name="login")
+    path('delete-task/<int:pk>/', TaskDelete.as_view(), name="delete-task")
     ]
--- todo_project/todoapp/views.py
+++ todo_project/todoapp/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.views import LoginView
 from .models import Task
 
 # Create your views here.
@@ -31,9 +30,3 @@ class TaskDelete(DeleteView):
     fields = "__all__"
     context_object_name = "tasks"
     success_url = reverse_lazy("task")
-
-class TaskListLoginView(LoginView):
-    fields = "__all__"
-    template_name = 'todoapp/login.html'
-    def get_success_url(self):
-        return reverse_lazy('tasks')
