655d3c79a5bf77dba107bdc08bf6de4cca306648
yukishi24 <yukishi24github@gmail.com>
2025-02-23 15:02:22 +0900

    create: detail pages

--- todo_project/todoapp/urls.py
+++ todo_project/todoapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view()),
-]
+    path('taskDetail/<int:pk>/', TaskDetail.as_view(), name="task")]
--- todo_project/todoapp/views.py
+++ todo_project/todoapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic import DetailView
 from .models import Task
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
     context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
