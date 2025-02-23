73bf896970f9c401ebfa48f2aab95566b23de2d8
yukishi24 <yukishi24github@gmail.com>
2025-02-23 16:13:36 +0900

    create delete page

--- todo_project/todoapp/views.py
+++ todo_project/todoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 
 # Create your views here.
@@ -19,3 +19,14 @@ class TaskCreate(CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__"
+    context_object_name = "tasks"
+    success_url = reverse_lazy("task")
