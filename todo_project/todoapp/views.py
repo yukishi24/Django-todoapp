f612d63ceec3ba37c141df9f115de45919edfe61
yukishi24 <yukishi24github@gmail.com>
2025-02-23 15:52:41 +0900

    create create pages

--- todo_project/todoapp/views.py
+++ todo_project/todoapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 # Create your views here.
@@ -12,3 +14,8 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
